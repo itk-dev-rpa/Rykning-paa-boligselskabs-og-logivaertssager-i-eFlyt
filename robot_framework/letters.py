5a8650e565db724b26ad24ce4430db41d4b091c4
Mathias G <ghbm@aarhus.dk>
2023-12-22 11:20:24 +0100

    Letters added

---
+++ robot_framework/letters.py
@@ -0,0 +1,11 @@
+LETTER_TO_ANMELDER = """Du har anmeldt flytning til ovenstående adresse.
+
+Vi har sendt et brev til logiværten, der bor på adressen, eller som er registreret som ejer af ejendommen.
+
+Pågældende har ikke svaret på brevet. Din flytning til adressen kan ikke blive godkendt, før den er bekræftet af en lejer/ejer af boligen.
+
+"""
+
+LETTER_TO_ANMELDER_BOLIGSELSKAB = "Vi vil bede dig om at kontakte Kontrolteamet inden 8 dage. Du kan ringe på telefonnummer 89404132 eller 89404113 mandag til fredag fra kl. 10 til 13."
+
+LETTER_TO_ANMELDER_LOGIVAERT = "Vi vil bede dig om at kontakte Folkeregisteret inden 8 dage. Du kan ringe på telefonnummer 89401030  Mandag, onsdag og fredag Kl. 10:00 - 14:00, Torsdag: kl. 10:00 -16:00, Tirsdag: Lukket."
